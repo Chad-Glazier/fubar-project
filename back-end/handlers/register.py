9e54373b025f82cad21be94cc6af95bf9a9b42ee
SamWuzhere <Samueliyogun10@gmail.com>
2025-11-16 15:42:38 -0800

    worked on handlers login and register

---
+++ back-end/handlers/register.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+import bcrypt
+
+from models.User import User
+
+register_router = APIRouter(prefix="/auth", tags=["auth"])
+
+class RegisterBody(BaseModel):
+    name: str
+    email: str
+    password: str
+
+@register_router.post("/register")
+async def register_user(body: RegisterBody):
+    existing_user = User.find_one({"email": body.email})
+    if existing_user:
+        raise HTTPException(status_code=404, detail="Email already registered.")
+
+    hashed_pw = bcrypt.hashpw(body.password.encode(), bcrypt.gensalt()).decode()
+
+    new_user = User(
+        id=User.generate_pk(),
+        name=body.name,
+        email=body.email,
+        password=hashed_pw,
+    )
+
+    new_user.save()
+
+    user_dict = new_user.to_dict()
+    user_dict.pop("password", None)
+
+    return {"message": "User registered", "user": user_dict}
--- back-end/server.py
+++ back-end/server.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, HTTPException
 from db import User
 
-from handlers import user_router
+from handlers import user_router, register_router,login_router
 
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(register_router)
+app.include_router(login_router)
