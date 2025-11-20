2b1880a4e3f98760195c2e01ed1d321fab6550e4
SamWuzhere <Samueliyogun10@gmail.com>
2025-11-19 23:49:19 -0800

    feat: add book details endpoint (GET /books/{id}) + tests

--- back-end/handlers/__init__.py
+++ back-end/handlers/__init__.py
@@ -1,1 +1,10 @@
 from .user import user_router
+from .search import search_router
+from .recommendations import recommend_router
+from .book_details import book_router
+
+from .search import search_router
+from .recommendations import recommend_router
+from .book_details import book_router
+
+__all__ = ["user_router", "search_router", "recommend_router", "book_router"]
--- back-end/server.py
+++ back-end/server.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI, HTTPException
 from db.models import User
 
-from handlers import user_router
+from handlers import user_router, search_router, recommend_router, book_router
+
 
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(search_router)
+app.include_router(recommend_router)
+app.include_router(book_router)
