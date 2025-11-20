ea991b84af76fefb0d8b9f6aeadd1b9ea4b3d7c6
SamWuzhere <Samueliyogun10@gmail.com>
2025-11-19 21:49:50 -0800

    made get/search endpoint  and made search class

--- back-end/handlers/seach.py
+++ back-end/handlers/seach.py
@@ -14,4 +14,22 @@ async def search_books(
     if not all_books:
         raise HTTPException(status_code=404, detail="No books found")
     
+     # Filter step-by-step
+    results = all_books
+
+    if author:
+        results = [book for book in results if author.lower() in book.author.lower()]
     
+    if year:
+        results = [book for book in results if book.year == year]
+
+    if rating_min:
+        results = [book for book in results if book.average_rating >= rating_min]
+
+    if rating_max:
+        results = [book for book in results if book.average_rating <= rating_max]
+
+    if len(results) == 0:
+        raise HTTPException(status_code=404, detail="No matching books found")
+
+    return results
--- back-end/server.py
+++ back-end/server.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 from handlers import user_router
+from handlers.search import search_router
+
 
 app = FastAPI()
-
+app.include_router(search_router)
 app.include_router(user_router)
