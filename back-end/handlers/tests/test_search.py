748efb23d85839358ee4bfb1b48f98fef0b227d1
SamWuzhere <Samueliyogun10@gmail.com>
2025-11-22 15:13:40 -0800

    Fix search handler tests after rebase

--- back-end/handlers/tests/test_search.py
+++ back-end/handlers/tests/test_search.py
@@ -25,19 +25,6 @@ def with_temp_books(func: Callable[[TestClient], None]):
 def test_search_returns_results_without_auth(client: TestClient):
     Book(id="s1", title="Alpha", authors=["Author"], average_rating=8).put()
     Book(id="s2", title="Beta", authors=["Author"], average_rating=6).put()
-def test_search_returns_results_without_auth():
-    client = TestClient(app)
-    original_dir = Book.data_dir
-    Book.data_dir = "./data/testing-data"
-    Book._drop_table()
-
-    books = [
-        Book(id="s1", title="Alpha", authors=["Author"], average_rating=8),
-        Book(id="s2", title="Beta", authors=["Author"], average_rating=6),
-    ]
-    for book in books:
-        book.put()
-
     resp = client.get("/search?limit=1&rating_min=7")
     assert resp.status_code == 200
     data = resp.json()
@@ -66,5 +53,3 @@ def test_search_returns_404_when_no_match(client: TestClient):
 
     resp = client.get("/search?author=Missing")
     assert resp.status_code == 404
-    Book._drop_table()
-    Book.data_dir = original_dir
