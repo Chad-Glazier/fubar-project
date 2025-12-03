a7b536aa749c459c7417253ad16d824227cfa9de
Chad Glazier <chadglazier@outlook.com>
2025-12-02 23:14:17 -0800

    working on profile page

---
+++ front-end/pages/profile/index.tsx
@@ -0,0 +1,10 @@
+import styles from "@/styles/Profile.module.css"
+import server from "@/lib/server"
+import { useState, useEffect } from "react"
+import Head from "next/head"
+import { useRouter } from "next/router"
+import useUser from "@/lib/hooks/useUser"
+
+export default function Profile() {
+	
+}
